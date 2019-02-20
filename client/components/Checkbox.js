2165caa841ddb4c77a3299fd8095578222390262
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-02-20 14:52:17 -0500

    git gud

---
+++ client/components/Checkbox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Checkbox = ({name, town, checked, onChange}) => {
+  return (
+    <div
+      className="checkbox"
+      onClick={town ? () => onChange(name, town) : () => onChange(name)}
+    >
+      <div className={`${checked ? 'checked' : 'unchecked'}`} />
+      <div className={`${checked ? 'name-checked' : 'name-unchecked'}`}>
+        {name}
+      </div>
+    </div>
+  )
+}
+
+export default Checkbox
